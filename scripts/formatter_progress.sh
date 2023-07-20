9f5b6b8d234b629a0fb92af979deb2e3a1035f5d
konstin <konstin@mailbox.org>
2023-07-20 15:20:00 +0200

    Style improvements

--- scripts/formatter_progress.sh
+++ scripts/formatter_progress.sh
@@ -19,7 +19,7 @@ if [ ! -d "$dir/warehouse" ]; then git clone https://github.com/pypi/warehouse "
 # django project
 if [ ! -d "$dir/zulip" ]; then git clone https://github.com/zulip/zulip "$dir/zulip"; fi
 
-for i in "$dir"/*/; do (cd $i && echo "# $(basename $i) $(git rev-parse HEAD)"); done
+for i in "$dir"/*/; do echo "# $(basename $i) $(git -C "$i" rev-parse HEAD)"; done
 # build 5800521541e5e749d4429617420d1ef8cdb40b46
 # django 0016a4299569a8f09ff24053ff2b8224f7fa4113
 # transformers 5bb4430edc7df9f9950d412d98bbe505cc4d328b
@@ -27,5 +27,6 @@ for i in "$dir"/*/; do (cd $i && echo "# $(basename $i) $(git rev-parse HEAD)");
 # warehouse e72cca94e7ac0dbe095db5c2942ad9f2f51b30cc
 # zulip 1cd587d24be1d668fcf6d136172bfec69e35cb75
 
-cargo run --bin ruff_dev -- format-dev --multi-project "$dir" > "$target/progress_projects_report.txt"
-grep "similarity index" target/progress_projects_report.txt | sort
+cargo run --bin ruff_dev -- format-dev --stability-check --error-file "$target/progress_projects_errors.txt" \
+  --multi-project "$dir" >"$target/progress_projects_report.txt"
+grep "similarity index" "$target/progress_projects_report.txt" | sort
